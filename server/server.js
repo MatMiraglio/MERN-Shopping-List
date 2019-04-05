91092344de0a1d2b30627bc1a495e9aef5a340fd
Matias <miragliomatias@gmail.com>
2019-04-05 14:15:53 +0200

    create items endpoints

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+const items = require('./routes/api/items')
+
 const app = express()
 app.use(bodyParser.json())
 
@@ -13,6 +15,9 @@ mongoose.connect(db)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err))
 
-const port = process.env.PORT || 5000
+//Use Routes
+app.use('/api/items', items)
 
+
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server started in port ${port}`))
